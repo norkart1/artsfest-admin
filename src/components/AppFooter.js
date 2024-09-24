8c17df5586834653f4149a984fd3421215fc142d
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-09-24 06:50:06 +0530

    minutes

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,15 +5,15 @@ const AppFooter = () => {
   return (
     <CFooter className="px-4">
       <div>
-        <a href="http://Fx review/" target="_blank" rel="noopener noreferrer">
-          The FX review
+        <a href="http://..?" target="_blank" rel="noopener noreferrer">
+          Fest 2024
         </a>
         <span className="ms-1">&copy; 2024 creativeLabs.</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
-        <a href="http://dhanwis.com/" target="_blank" rel="noopener noreferrer">
-          Dhanwis TechInfo Solution
+        <a href="https://corvoso.vercel.app/" target="_blank" rel="noopener noreferrer">
+          Corvoso Technologies
         </a>
       </div>
     </CFooter>
