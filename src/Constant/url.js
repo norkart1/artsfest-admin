d5255151babb98a9a451b77d771f0ab2eb9f416f
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-11-16 22:40:56 +0530

    url updation

--- src/Constant/url.js
+++ src/Constant/url.js
@@ -2,8 +2,3 @@ export const adminBaseUrl = "https://sargalayamserver-production.up.railway.app/
 export const teamBaseUrl = "https://sargalayamserver-production.up.railway.app/teams";
 export const imageUrl = "https://sargalayamserver-production.up.railway.app/teamImages";
 export const imageUrl2 = "https://sargalayamserver-production.up.railway.app/programImg";
-
-// export const adminBaseUrl = 'https://sargalayamserver.onrender.com/admin'
-// export const teamBaseUrl = 'https://sargalayamserver.onrender.com/teams'
-// export const imageUrl = 'https://sargalayamserver.onrender.com/teamImages'
-// export const imageUrl2 = 'https://sargalayamserver.onrender.com/programImg'
